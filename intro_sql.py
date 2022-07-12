f7ecba01994a878090ba696af6f897b923cccd6d
Usman Musa <usmanmusa1920@gmail.com>
2022-07-12 03:06:45 +0100

    Number one

---
+++ intro_sql.py
@@ -0,0 +1,15 @@
+"""
+    SQL (STRUCTURED QUERY LANGUAGE)
+    
+    In this chapter we are going to see how and the power of SQL, but first what is SQL? SQL stands for (Structured Query Language) it is a standard language for storing, manipulating and retrieving data in database. By convention SQL keywords are use all in uppercase e.g SELECT, UPDATE, DELETE, etc, in other to make it easy to read, although it is case-sensitive. Also we are going to take our hands step by step to know how to use MYSQL, POSTGRESQL, and possibly some others, so stay tuned. Before we dive deep we have to know a little bit about the above mention databases (mysql, postgreql).
+    
+    MySQL
+    MySQL is a free and open source relational database management system (RDBMS) which was developed by Micheal Widenius in 1995. Mysql is a database server that manages the databases and tables, controls user access and processess the SQL queries. You can use mysql whether on the terminal, mysql work bench, phpMyAdmin or others.
+    
+    PostgreSQL
+    Postgresql is a free and open source relational databse management system (RDBMS) developed in 1996. It store data points in rows, with columns as different data attributes. A table stores multiple related rows. The relational database is the most common type of database in use. It differentiates itself with a focus on integrations and extensibility.
+    
+    SELECT * FROM engineers;
+    SELECT occupation FROM engineers WHERE occupation="Software engineer";
+
+"""
